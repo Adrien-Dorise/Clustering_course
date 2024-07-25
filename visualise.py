1499580d9eea8bbdd2c7a18b163f3df7cd89bd5a
Adrien-Dorise <adrien.dorise@hotmail.com>
2024-07-25 23:23:14 +0200

    Updated legend position when plotting clutering

--- visualise.py
+++ visualise.py
@@ -7,7 +7,7 @@ def plot_clustering(dataset, labels, title="Clustering result"):
     f1 = [x[1] for x in dataset]
     fig = plt.scatter(f0, f1, c=labels, cmap="tab20")
     lab = [str(x) for x in np.unique(labels)]
-    plt.legend(handles=fig.legend_elements()[0], labels=lab, title="clusters")
+    plt.legend(handles=fig.legend_elements()[0], labels=lab, title="clusters", loc='upper right')
     plt.grid(True)
     plt.title(title)
     plt.xlabel("Feature 1")
